4d695fdea4c2a8ec62b02bd7137ccaba2dd12091
Rafael JPD <rafael.pezzuto@gmail.com>
2021-08-30 19:11:59 -0300

    adiciona métodos para impressão de cabeçalho e resultados

--- validate.py
+++ validate.py
@@ -26,3 +26,49 @@ PATTERN_IP_DATETIME_OTHERS = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - - \[(.*)\
 PATTERN_IP_DATETIME_RESOUCE_STATUS_LENGHT_REFERRER_EQUIPMENT = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - - \[(.*)\] \"GET (.*) .*\" (\d{3}) (\d*) \"(.*)\" \"(.*)\"$'
 
 
+def _print_header():
+    print(app_msg)
+
+    header = '\t'.join([
+        'file_path',
+        'file_date',
+        'file_collection',
+        'file_type',
+        'file_name_is_paperboy',
+        'valid_ip',
+        'valid_date',
+        'invalid_lines',
+        'total_lines',
+        'ip_local',
+        'ip_remote'
+    ])
+    print(header)
+
+
+def print_results(results, file_path):
+    line = '\t'.join(
+        [file_path] +
+        [
+            str(results['validate_path']['results'][x]) for x in [
+                'file_name_date',
+                'file_name_collection',
+                'file_type',
+                'file_name_has_paperboy_format']
+        ] +
+        [
+            str(results['valid_ip']),
+            str(results['valid_date'])
+        ] +
+        [
+            str(results['validate_content']['results']['log_content'][z]) for z in [
+                'invalid_lines',
+                'total_lines']
+        ] +
+        [
+            str(results['validate_content']['results']['log_content']['ip'][k]) for k in [
+                'local',
+                'remote'
+            ]
+        ]
+    )
+    print(line)
