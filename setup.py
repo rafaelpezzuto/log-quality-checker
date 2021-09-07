27d7f91728f28840ff0302c99ad6da9f16488190
Rafael JPD <rafael.pezzuto@gmail.com>
2021-09-07 17:41:50 -0300

    adiciona setup.py

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python2.7
+import os, setuptools
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, "README.md")) as f:
+    README = f.read()
+
+requires = [
+    'ipaddress',
+    'python-magic',
+    'six',
+]
+
+tests_require = [
+]
+
+setuptools.setup(
+    name="log-quality-checker",
+    version="0.1",
+    author="SciELO",
+    author_email="scielo-dev@googlegroups.com",
+    description="",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="2-clause BSD",
+    packages=setuptools.find_packages(
+        exclude=["*.tests", "*.tests.*", "tests.*", "tests"]
+    ),
+    include_package_data=True,
+    extras_require={"testing": tests_require},
+    install_requires=requires,
+    dependency_links=[
+    ],
+    python_requires=">=2.7",
+    test_suite="tests",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Other Environment",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 2 :: Only",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "Operating System :: OS Independent",
+    ],
+)
