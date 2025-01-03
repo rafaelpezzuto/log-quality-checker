58c7b8876b0126f8d617a4dcfa71c4dc419f8491
Rafael JPD <rafael@pitangainovare.com.br>
2025-01-03 17:53:26 -0300

    Cria teste para arquivo principal de validação

---
+++ tests/test_validator.py
@@ -0,0 +1,12 @@
+import datetime
+import unittest
+
+from scielo_log_validator import validator
+
+
+class TestValidator(unittest.TestCase):
+
+    def setUp(self):
+        self.log_directory = 'tests/fixtures/logs/scielo.wi/'
+        self.log_file = 'tests/fixtures/logs/scielo.wi/2024-02-20_caribbean.scielo.org.1.log.gz'
+        self.log_file_invalid_name = 'tests/fixtures/logs/scielo.wi/invalid_file_name.log.gz'
