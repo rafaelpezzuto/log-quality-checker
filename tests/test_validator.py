16337a4a78f6de74e39ca9a929a0f1bf2789b30a
Rafael JPD <rafael@pitangainovare.com.br>
2025-01-03 18:59:49 -0300

    Adiciona mais testes para método pipeline_validate

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -8,11 +8,12 @@ class TestValidator(unittest.TestCase):
 
     def setUp(self):
         self.log_directory = 'tests/fixtures/logs/scielo.wi/'
-        self.log_file = 'tests/fixtures/logs/scielo.wi/2024-02-20_caribbean.scielo.org.1.log.gz'
+        self.log_file = 'tests/fixtures/logs/scielo.scl/2022-03-05_scielo-br.log.gz'
+        self.log_file_invalid_content = 'tests/fixtures/logs/scielo.wi/2024-02-20_caribbean.scielo.org.1.log.gz'
         self.log_file_invalid_name = 'tests/fixtures/logs/scielo.wi/invalid_file_name.log.gz'
 
     def test_get_execution_mode_is_file(self):
-        exec_mode = validator.get_execution_mode(self.log_file)
+        exec_mode = validator.get_execution_mode(self.log_file_invalid_content)
         self.assertEqual(exec_mode, 'validate-file')
 
     def test_get_execution_mode_is_directory(self):
@@ -30,7 +31,7 @@ class TestValidator(unittest.TestCase):
         self.assertEqual((y, m, d, h), (2023, 3, 12, 14))
 
     def test_count_lines(self):
-        obtained_nlines = validator.get_total_lines(self.log_file)
+        obtained_nlines = validator.get_total_lines(self.log_file_invalid_content)
         expected_nlines = 7160
         self.assertEqual(obtained_nlines, expected_nlines)
 
@@ -104,11 +105,11 @@ class TestValidator(unittest.TestCase):
         self.assertIn('extension', results)
 
     def test_validate_content(self):
-        results = validator.validate_content(self.log_file)
+        results = validator.validate_content(self.log_file_invalid_content)
         self.assertIn('summary', results)
 
-    def test_validate_results_false(self):
-        obtained_results = validator.pipe_validate(self.log_file)
+    def test_pipeline_validate_successfully_runs(self):
+        obtained_results = validator.pipeline_validate(self.log_file_invalid_content)
         expected_results = {
             'path': {
                 'date': '2024-02-20', 
@@ -157,8 +158,13 @@ class TestValidator(unittest.TestCase):
         self.assertDictEqual(obtained_results, expected_results)
         self.assertFalse(obtained_results['is_valid']['all'])
 
-    def test_validate_only_path_is_false(self):
-        obtained_results = validator.pipe_validate(self.log_file, apply_path_validation=True, apply_content_validation=False)
+    def test_pipeline_validate_only_path(self):
+        obtained_results = validator.pipeline_validate(
+            path=self.log_file_invalid_content, 
+            apply_path_validation=True, 
+            apply_content_validation=False
+        )
+
         expected_results = {
             'path': {
                 'date': '2024-02-20', 
@@ -173,6 +179,65 @@ class TestValidator(unittest.TestCase):
             }
         self.assertDictEqual(obtained_results, expected_results)
         self.assertFalse(obtained_results['is_valid']['all'])
+
+    def test_pipeline_validate_only_content(self):
+        obtained_results = validator.pipeline_validate(
+            path=self.log_file_invalid_content, 
+            apply_path_validation=False, 
+            apply_content_validation=True
+        )
+
+        expected_results = {
+            'is_valid': {'ips': False, 'dates': False, 'all': False}, 
+            'probably_date': datetime.datetime(2024, 2, 21, 0, 0),
+            'content': {
+                'summary': {
+                    'datetimes': {
+                        (2024, 2, 21, 0): 22, 
+                        (2024, 2, 21, 1): 34, 
+                        (2024, 2, 21, 2): 21, 
+                        (2024, 2, 21, 3): 35, 
+                        (2024, 2, 21, 4): 29, 
+                        (2024, 2, 21, 5): 22, 
+                        (2024, 2, 21, 6): 29, 
+                        (2024, 2, 21, 7): 43, 
+                        (2024, 2, 21, 8): 29, 
+                        (2024, 2, 21, 9): 30, 
+                        (2024, 2, 21, 10): 35, 
+                        (2024, 2, 21, 11): 25, 
+                        (2024, 2, 21, 12): 27, 
+                        (2024, 2, 21, 13): 31, 
+                        (2024, 2, 21, 14): 31, 
+                        (2024, 2, 21, 15): 29, 
+                        (2024, 2, 21, 16): 23, 
+                        (2024, 2, 21, 17): 24, 
+                        (2024, 2, 21, 18): 29, 
+                        (2024, 2, 21, 19): 28, 
+                        (2024, 2, 21, 20): 37, 
+                        (2024, 2, 21, 21): 33, 
+                        (2024, 2, 21, 22): 33, 
+                        (2024, 2, 21, 23): 36,
+                    },
+                    'ips': {'local': 700, 'remote': 15}, 
+                    'invalid_lines': 0,
+                    'total_lines': 7160,
+                }
+            }, 
+        }
+        self.assertDictEqual(obtained_results, expected_results)
+        self.assertFalse(obtained_results['is_valid']['all'])
+
+    def test_pipeline_validate_with_sample_size_zero(self):
+        obtained_results = validator.pipeline_validate(self.log_file, sample_size=0)
+        self.assertTrue(obtained_results['is_valid']['all'])
+
+    def test_pipeline_validate_with_sample_size_greater_than_one(self):
+        obtained_results = validator.pipeline_validate(self.log_file_invalid_content, sample_size=100)
+        self.assertTrue(obtained_results['is_valid']['dates'])
+
+    def test_pipeline_validate_with_directory(self):
+        obtained_results = validator.pipeline_validate(self.log_file_invalid_content, sample_size=100)
+        self.assertTrue(obtained_results['is_valid']['dates'])
 
     def test_get_date_frequencies(self):
         results = {
